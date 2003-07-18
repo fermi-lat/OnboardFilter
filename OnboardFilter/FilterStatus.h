f0c8e1b2a60e02f0a013b2d55bc0e1684c0c8841
golpa <>
2003-07-18 20:22:37 +0000

    Added Energy in CAL to the TDS

--- OnboardFilter/FilterStatus.h
+++ OnboardFilter/FilterStatus.h
@@ -9,6 +9,7 @@
 #include "GaudiKernel/ObjectVector.h"
 
 #include "Event/TopLevel/Definitions.h"
+#include "Event/TopLevel/EventModel.h"
 
 /**
  * @class FilterStatus
@@ -16,11 +17,13 @@
  */
 extern const CLID& CLID_FilterStatus;
 
-namespace Event{
+
+namespace OnboardFilterTds{
+
   class FilterStatus : public DataObject{
   public:
     FilterStatus();
-    FilterStatus(const unsigned int code);
+    FilterStatus(const unsigned int code,const int energy=0);
     virtual ~FilterStatus();
     static const CLID& classID() { return CLID_FilterStatus; }
     virtual const CLID& clID() { return classID(); }
@@ -37,11 +40,19 @@ namespace Event{
     ///Returns the 16 least signifcat bits of the filter code
     unsigned int getLow() const;
 
+    ///Set the Energy in CAL
+      void setCalEnergy(const int energy);
+
+    ///Returns the value stored in CalEnergy
+    int getCalEnergy();
+	
     virtual std::ostream& fillStream(std::ostream &s) const;
     friend std::ostream& operator << (std::ostream &s, const FilterStatus &obj);
   private:
     ///Filter status code
     unsigned int status;
+    ///Energy in CAL
+    int CalEnergy;
   };
 };
 #endif
