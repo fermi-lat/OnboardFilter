94cac5ee430c4914e3ee4046cda17aa1ee63bea0
burnett <>
2003-08-17 22:40:41 +0000

    more inlines; tagging v0r1p1

--- OnboardFilter/FilterStatus.h
+++ OnboardFilter/FilterStatus.h
@@ -161,7 +161,109 @@ namespace OnboardFilterTds{
         return m_layers;
     }
 
+    // the put methods, here for now
+    inline FilterStatus::FilterStatus(){
+      m_status=0;
+      m_calEnergy=0;
+      m_tcids=0;
+      m_acd_xy=0;
+      m_acd_xz=0;
+      m_acd_yz=0;
+      for(int counter=0;counter<16;counter++){
+		m_acdStatus[counter]=0;
+		m_layers[counter]=0;
+      }
+    }
+    inline FilterStatus::FilterStatus(const unsigned int code, const int energy,
+						   const int ids, const int xz, const int yz,
+						   const int xy, const int *acdstatus,
+						   const int *layerCode,
+						   const TFC_projections *projections){
+      m_status=code;
+      m_calEnergy=energy;
+      m_tcids=ids;
+      m_acd_xz=xz;
+      m_acd_xy=xy;
+      m_acd_yz=yz;
+      for(int counter=0;counter<16;counter++){
+		m_acdStatus[counter]=0;
+		m_layers[counter]=0;
+      }
+      if(acdstatus!=NULL){
+	for(int counter=0;counter<16;counter++)
+	  m_acdStatus[counter]=acdstatus[counter];
+      }
+      if(layerCode!=NULL){
+        for(int counter=0;counter<16;counter++)
+	  m_layers[counter]=layerCode[counter];
+      }
+      if(projections!=NULL){
+	for(int counter=0;counter<16;counter++){
+	  for(int trackCounter=0;trackCounter<projections[counter].curCnt;trackCounter++){
+	    m_prjs[counter].push_back(projections[counter].prjs[trackCounter]);
+	  }
+	}
+      }
+    }
+
+    inline FilterStatus::~FilterStatus(){
+    }
+
+    inline void FilterStatus::set(const unsigned int code){
+      m_status=code;
+    }
+
+    inline void FilterStatus::setCalEnergy(const int energy){
+      m_calEnergy=energy;
+    }
 
 
-};
+    inline void FilterStatus::setTcids(const int ids){
+      m_tcids=ids;
+    }
+
+
+    inline void FilterStatus::setAcdMap(const int xz, const int yz, const int xy){
+      m_acd_xz=xz;
+      m_acd_yz=yz;
+      m_acd_xy=xy;
+    }
+
+
+
+    inline void FilterStatus::setAcdStatus(const int tower, const int status){
+      if(tower<16)
+        m_acdStatus[tower]=status;
+    }
+
+
+    inline void FilterStatus::setLayers(const int *layerCode){
+      for(int counter=0;counter<16;counter++)
+        m_layers[counter]=layerCode[counter];
+    }
+
+
+    inline void FilterStatus::setProjection(const int tower,const TFC_projections prjs){
+      if(tower<16){
+	m_prjs[tower].clear();
+	for(int counter=0;counter<prjs.curCnt;counter++)
+	  m_prjs[tower].push_back(prjs.prjs[counter]);
+      }
+    }
+
+
+    inline std::ostream& FilterStatus::fillStream(std::ostream &s) const{
+      s<<"Filter Return Code: "<<m_status<<std::endl;
+      s<<"Filter code for Energy in CAL: "<<m_calEnergy<<std::endl;
+      return s;
+    }
+
+
+inline std::ostream& operator<<(std::ostream &s, const FilterStatus &obj){
+  return obj.fillStream(s);
+}
+
+}//namespace OnboradFilterTds 
+
+
 #endif
