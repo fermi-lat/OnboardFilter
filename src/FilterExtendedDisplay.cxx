323e06eba37a0bc4bac86da9b7f118921dec7f13
golpa <>
2003-08-21 19:36:07 +0000

    New classes to handle displaying

---
+++ src/FilterExtendedDisplay.cxx
@@ -0,0 +1,22 @@
+#include "FilterExtendedDisplay.h"
+#include <vector>
+
+FilterExtendedDisplay::FilterExtendedDisplay(IDataProviderSvc *service){
+  dps=service;
+}
+
+void FilterExtendedDisplay::update(){
+  OnboardFilterTds::FilterStatus *status=SmartDataPtr
+    <OnboardFilterTds::FilterStatus>(dps,"/Event/Filter/FilterStatus");
+  if(status){
+    setColor("green");
+    vector<OnboardFilterTds::track> tracks=status->getTracks();
+    for(int counter=0;counter<tracks.size();counter++){
+      set_line_style(DOTTED_LINE);
+      moveTo(tracks[counter].exLowCoord[0],tracks[counter].exLowCoord[1],
+	     tracks[counter].exLowCoord[2]);
+      lineTo(tracks[counter].exHighCoord[0],tracks[counter].exHighCoord[1],
+	     tracks[counter].exHighCoord[2]);
+    }
+  }
+}
