b5122311023883c00f4d2426ab53c5ce8e46892b
burnett <>
2005-09-06 16:42:29 +0000

    updates for Windows

---
+++ src/Dll/OnboardFilter_dll.cxx
@@ -0,0 +1,9 @@
+/** @file OnboardFilter_dll.cxx
+
+  $Header: /nfs/slac/g/glast/ground/cvs/OnboardFilter/src/Dll/OnboardFilter_dll.cxx,v 1.3 2004/07/12 19:47:27 golpa Exp $
+*/
+
+// All that is needed in this file is this include
+// and the one line LOAD_FACTORY_ENTRIES(packageName)
+#include "GaudiKernel/LoadFactoryEntries.h"
+LOAD_FACTORY_ENTRIES(OnboardFilter)
---
+++ src/Dll/OnboardFilter_load.cxx
@@ -0,0 +1,18 @@
+/** 
+* @file OnboardFilter_load.cxx
+* @brief This is needed for forcing the linker to load all components
+* of the library.
+*
+*  $Header: /nfs/slac/g/glast/ground/cvs/OnboardFilter/src/Dll/OnboardFilter_load.cxx,v 1.5 2004/07/12 19:47:27 golpa Exp $
+*/
+
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+// There should be one entry for each component included in 
+// the library for this package.
+DECLARE_FACTORY_ENTRIES(OnboardFilter) {
+    DECLARE_ALGORITHM(OnboardFilter);
+    DECLARE_ALGORITHM(FilterTracks);
+	DECLARE_ALGORITHM(FilterAlg);
+    DECLARE_TOOL(FilterDisplay);
+} 
