445ded9783a813d10dfe890f92938c77dd4f6157
usher <>
2007-09-18 19:28:09 +0000

    Fix mechanism for setting "pass through" for gamma filter, should fix discrepancy seen in gamma filter system tests

--- src/ObfInterface.cxx
+++ src/ObfInterface.cxx
@@ -45,24 +45,35 @@ static int          passThrough (void *unused, unsigned int nbytes, EBF_pkt *pkt
 
 /* ---------------------------------------------------------------------- */
 // Local struct definition for veto mask (must be better way...)
-typedef struct _EFC_samplerPrescaler
+typedef struct _EFC_samplerPrescaleCounter
 {
   unsigned int countdown;   /*!< Current countdown value                  */
   unsigned int   refresh;   /*!< Refresh value                            */
-} EFC_samplerPrescaler;
+} EFC_samplerPrescaleCounter;
 
-typedef struct _EFC_samplerVetoes
+typedef struct _EFC_samplerPrescaleCtx
 {
-  unsigned int      def;   /*!< Default veto mask                         */
-  unsigned int   active;   /*!< Active (this event) veto mask             */
-} EFC_samplerVetoes;
+    EFC_samplerPrescaleCounter
+                         in;    /*!< Input prescaler                      */
+    EFC_samplerPrescaleCounter
+                        out;    /*!< Output prescaler                     */
+    EFC_classes     enabled;    /*!< The classes of enabled prescalers    */
+    EFC_samplerPrescaleCounter
+              prescalers[32];   /*!< The prescales for each status bit
+                                     an additional one for the output 
+                                     prescaling                           */
+} EFC_samplerPrescaleCtx;
+
+typedef struct _EFC_samplerClasses
+{
+    EFC_classes   defined; /*!< The set of defined class of bits          */
+    EFC_classes   enabled; /*!< The set so enabled class of bits          */
+} EFC_samplerClasses;
 
 typedef struct _EFC_sampler
 {
-   EFC_samplerVetoes vetoes; /*!< The default and active veto mask        */
-   unsigned int     enables; /*!< The mask of enabled samplers            */
-   EFC_samplerPrescaler
-             prescalers[32]; /*!< The prescales for each veto condition   */
+    EFC_samplerPrescaleCtx  prescale; /*!< The prescale context           */
+    EFC_samplerClasses       classes; /*!< The status word classes        */
 } EFC_sampler;
 
 /* ---------------------------------------------------------------------- */
@@ -217,7 +228,7 @@ int ObfInterface::setupFilter(const std::string& filterName, int priority, unsig
 //        {
             EFC_sampler* sampler = (EFC_sampler*)EFC_get(filter, EFC_OBJECT_K_SAMPLER);
 
-            sampler->vetoes.def = vetoMask;
+            sampler->classes.enabled.all = vetoMask;
 
 //            sampler->vetoes.def = 0;
 //        }
