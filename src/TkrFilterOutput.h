eb07d6bf0f417c6cc41a4a6d9c4ca509c509293f
usher <>
2008-01-09 20:57:53 +0000

    Modifications to include new GrbTracks class which uses FSW code extracted from the GRB filter for mating X and Y projections to form a best track

--- src/TkrFilterOutput.h
+++ src/TkrFilterOutput.h
@@ -8,7 +8,7 @@
 *
 * @authors T. Usher
 *
-* $Header: /nfs/slac/g/glast/ground/cvs/TkrRecon/src/PatRec/VectorLinks/VecPoint.h,v 0.1 2006/03/21 01:12:37 usher Exp $
+* $Header: /nfs/slac/g/glast/ground/cvs/OnboardFilter/src/TkrFilterOutput.h,v 1.1 2006/12/12 22:19:04 usher Exp $
 */
 
 #ifndef __TkrFilterOutput_H
@@ -17,6 +17,7 @@
 #include "OutputRtn.h"
 
 class trackProj;
+class GrbFindTrack;
 
 class TkrFilterOutput : virtual public OutputRtn
 {
@@ -35,7 +36,8 @@ private:
     void extractBestTrackInfo(OnboardFilterTds::FilterStatus* filterStatus, EDS_fwIxb *ixb);
     void extractTkrTwrHitInfo(OnboardFilterTds::TowerHits* towerHits, EDS_fwIxb *ixb);
 
-    trackProj* m_trackProj;
+    trackProj*    m_trackProj;
+    GrbFindTrack* m_grbTrack;
 };
 
 #endif // __ObfInterface_H
