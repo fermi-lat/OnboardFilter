d5b19cd67761a9a1d77a11850075a2183216b56c
jrb <>
2010-09-23 21:40:59 +0000

    add some library dependencies to match CMT build

--- OnboardFilterLib.py
+++ OnboardFilterLib.py
@@ -1,4 +1,4 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/OnboardFilter/OnboardFilterLib.py,v 1.1 2008/08/15 21:42:36 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/OnboardFilter/OnboardFilterLib.py,v 1.2 2008/10/22 17:12:28 ecephas Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['OnboardFilter'])
@@ -8,6 +8,11 @@ def generate(env, **kw):
     env.Tool('addLibrary', library = env['obfLibs'])
     env.Tool('addLibrary', library = env['gaudiLibs'])
     env.Tool('OnboardFilterTdsLib')
+    env.Tool('MootSvcLib')
+    env.Tool('CalibDataLib')
+    env.Tool('LdfEventLib')
+    env.Tool('EventLib')
+    env.Tool('facilitiesLib')
     env.Tool('addLibrary', library = env['rootLibs'])
 def exists(env):
     return 1;
