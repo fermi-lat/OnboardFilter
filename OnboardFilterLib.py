6fae3161f2f2cfcb0dcebcf30e2d27c35764c0e3
heather <>
2012-02-01 22:19:48 +0000

    pick up patch from HEAD OnboardFilterLib.py

--- OnboardFilterLib.py
+++ OnboardFilterLib.py
@@ -1,10 +1,10 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/OnboardFilter/OnboardFilterLib.py,v 1.2 2008/10/22 17:12:28 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/OnboardFilter/OnboardFilterLib.py,v 1.4 2010/10/01 23:35:33 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['OnboardFilter'])
- 
-    env.Tool('addLibrary', library = ['dl'])
-    env.Tool('addLibrary', library = ['pthread'])
+    if not env['PLATFORM']  == 'win32':
+        env.Tool('addLibrary', library = ['dl'])
+        env.Tool('addLibrary', library = ['pthread'])
     env.Tool('addLibrary', library = env['obfLibs'])
     env.Tool('addLibrary', library = env['gaudiLibs'])
     env.Tool('OnboardFilterTdsLib')
--- SConscript
+++ SConscript
@@ -1,7 +1,7 @@
 # -*- python -*-
-# $Header: /nfs/slac/g/glast/ground/cvs/OnboardFilter/SConscript,v 1.16.36.2 2012/01/26 15:22:15 heather Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/OnboardFilter/SConscript,v 1.16.36.3 2012/01/31 21:41:15 heather Exp $
 # Authors: Tracy Usher <usher@SLAC.Stanford.edu>
-# Version: OnboardFilter-04-14-06-gr03
+# Version: OnboardFilter-04-14-06-gr04
 
 Import('baseEnv')
 Import('listFiles')
