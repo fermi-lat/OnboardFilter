b23085f18c19b78b43d254eb8d7db8e55a944bc8
jrb <>
2012-08-18 00:32:38 +0000

    support no-install headers on Windows

--- OnboardFilterLib.py
+++ OnboardFilterLib.py
@@ -1,6 +1,9 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/OnboardFilter/OnboardFilterLib.py,v 1.4 2010/10/01 23:35:33 jrb Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/OnboardFilter/OnboardFilterLib.py,v 1.5 2012/04/26 21:47:24 jrb Exp $
 def generate(env, **kw):
-    #if not kw.get('depsOnly', 0):
+    if not kw.get('depsOnly', 0):
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'OnboardFilter') 
+
     #    env.Tool('addLibrary', library = ['OnboardFilter'])
     if not env['PLATFORM']  == 'win32':
         env.Tool('addLibrary', library = ['dl'])
@@ -14,5 +17,9 @@ def generate(env, **kw):
     env.Tool('EventLib')
     env.Tool('facilitiesLib')
     env.Tool('addLibrary', library = env['rootLibs'])
+    if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package = 'EbfWriter') 
+        env.Tool('findPkgPath', package = 'GlastSvc') 
+
 def exists(env):
     return 1;
